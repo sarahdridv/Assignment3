250fb75237bb1036c807290b80dd7e082ff97cb9
sarahdridv <118977776+sarahdridv@users.noreply.github.com>
2022-12-06 13:37:39 +0100

    3 files modified

--- App.svelte
+++ App.svelte
@@ -1,28 +1,40 @@
 <script>
-  import { scaleOrdinal } from 'd3-scale'
-  import { schemePaired } from 'd3-scale-chromatic'
-  import { munis } from './munis.js'
-  import { Graphic, PolygonLayer, fitScales } from '@snlab/florence'
+  import { scaleLinear } from 'd3-scale'
+  import { csvParse, autoType } from 'd3-dsv'
+  import { Graphic, PointLayer, XAxis, YAxis } from '@snlab/florence'
   import DataContainer from '@snlab/florence-datacontainer'
-  
-  const munisContainer = new DataContainer(munis)
-  const geoScales = fitScales(munisContainer.domain("$geometry"))
 
+  let geodata
+  let scaleX
+  let scaleY
+  let done = false
+
+  async function loadCSV (url) {
+    const response = await fetch(url)
+    const data = await response.text()
+    const dataParsed = csvParse(data, autoType)
+    geodata = new DataContainer(dataParsed)
+    scaleX = scaleLinear().domain(geodata.domain('EL_AV_ALS'))
+    scaleY = scaleLinear().domain(geodata.domain('E_WR_P_00'))
+    done = true
+  }
+  loadCSV('data/Geography.csv')
+ 
 </script>
 
 <div class="graph">
   <div class="main-chart">
     <Graphic
-      {...geoScales}
-      flipY
+      {scaleX}
+      {scaleY}
       >
-      <PolygonLayer 
-      geometry={munisContainer.column("$geometry")}/>
-      
+      <PointLayer 
+      x={geodata.column('EL_AV_ALS')}
+      y={geodata.column('E_WR_P_00')}
+      fill={'blue'}/>
     </Graphic>
   </div>
 </div>
 
 <style>
-
 </style>
